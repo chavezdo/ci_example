8f9ff280aeed6090defee4d61a6644ce9f1295f0
chavezdo <122295223+chavezdo@users.noreply.github.com>
2023-02-21 07:39:17 -0800

    Add files via upload

--- task.py
+++ task.py
@@ -13,7 +13,7 @@ def conv_endian(num, endian='big'):
 def dec_to_hex(number):
     number = abs(number)
     table = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
-             5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 
+             5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
              10: 'A', 11: 'B', 12: 'C', 13: 'D', 
              14:  'E', 15: 'F'}
     number = abs(number)
